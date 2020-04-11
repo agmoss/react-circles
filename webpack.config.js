99dfa7cbea6df799302f1f2d51a56507c69d990a
Andrew Moss <andrew@m0ss.dev>
2020-04-10 21:00:26 -0600

    making my gist into a package

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  target: 'web',
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'index.js',
+    libraryTarget: 'umd',
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.ts', '.tsx'],
+    alias: {
+      react: path.resolve('./node_modules/react'),
+    },
+  },
+  module: {
+    rules: [{
+      test: /\.ts(x?)$/,
+      exclude: /node_modules/,
+      use: [{
+        loader: 'babel-loader',
+      }],
+    },
+    {
+      enforce: 'pre',
+      test: /\.js$/,
+      loader: 'source-map-loader',
+    },
+    ],
+  },
+  externals: {
+    react: 'umd react',
+  },
+};
