4c79281acd4ddac01372de61c43dabc370ed8f2d
Andrew Moss <andrew@m0ss.dev>
2020-06-20 17:32:42 -0600

    package tooling

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,28 +3,35 @@ module.exports = {
     browser: true,
     es6: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
   },
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
   parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 2018,
-    sourceType: 'module',
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
   rules: {
-    'no-plusplus': 'off',
-    "react/jsx-filename-extension": [0]
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/no-unused-expressions": "off",
+    "react/prop-types": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "react/display-name": "off",
+  },
+  plugins: ["@typescript-eslint", "react-hooks", "react"],
+  settings: {
+    react: {
+      version: "detect",
+    },
   },
 };
